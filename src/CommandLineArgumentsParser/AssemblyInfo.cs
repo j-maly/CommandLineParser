cb6eef87ff6cd4c92896d8486958e5fc478c5271
Stef Heyenrath <stef.heyenrath@gmail.com>
2017-08-22 21:47:11 +0200

    GeneratePackageOnBuild = true

--- src/CommandLineArgumentsParser/AssemblyInfo.cs
+++ src/CommandLineArgumentsParser/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jakub Maly")]
 [assembly: AssemblyProduct("Commandline Parser")]
-[assembly: AssemblyCopyright("Copyright Jakub Maly & Stef Heyenrath © 2016")]
+[assembly: AssemblyCopyright("Copyright Jakub Maly & Stef Heyenrath © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
